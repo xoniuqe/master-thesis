d940f1b5d1514dce7aa2c6d19a4be6722766b048
Tobias Arens <arenst@hochschule-trier.de>
2022-03-08 15:36:50 +0100

    playing around with gsl

--- SteepestDescent/SteepestDescent.cpp
+++ SteepestDescent/SteepestDescent.cpp
@@ -2,13 +2,73 @@
 //
 
 #include "SteepestDescent.h"
+#include  <complex> //include complex to replace the gsl complex numbers
+
 #include <gsl/gsl_linalg.h>
 
 
 using namespace std;
+void setup_1d_test()
+{
+	double A_data[]{ -5, 1, -1, 1, -1 ,0 };
+
+	double b_data[] = { 0,1,0 };
+	auto b = gsl_vector_view_array(b_data, 3);
+
+	double mu_data[] = { 1,4,0 };
+	//auto mu = gsl_vector_view_array(mu_data, 3);
+
+	auto A = gsl_matrix_alloc(2, 3);
+	for (auto i = 0; i < 2; i++) {
+		for (auto j = 0; j < 3; j++) {
+			gsl_matrix_set(A, i, j, A_data[i + (j * 2)]);
+		}
+	}
+	auto mu = gsl_vector_alloc(3);
+	for (auto j = 0; j < 3; j++) {
+		gsl_vector_set(mu, j, mu_data[j]);
+	}
+	auto A_1 = gsl_vector_alloc(3);
+	auto A_2 = gsl_vector_alloc(3);
+	for (auto j = 0; j < 3; j++) {
+		gsl_vector_set(A_1, j, A_data[j * 2]);
+		gsl_vector_set(A_2, j, A_data[1 + (j * 2)]);
+	}
+	std::cout << "A = \n";
+	gsl_matrix_fprintf(stdout, A, "%lf");
+	std::cout << "A(1) = \n";
+	gsl_vector_fprintf(stdout, A_1, "%lf");
+	std::cout << "A(2) = \n";
+	gsl_vector_fprintf(stdout, A_2, "%lf");
+
+	auto y = 0;
+
+	auto a_column_1 = gsl_matrix_column(A, 0);
+	auto a_column_2 = gsl_matrix_column(A, 1);
+	double q, s, s_1, s_2;
+	//gsl_blas_sdot
+	gsl_blas_ddot(mu, A_1, &q);
+	gsl_blas_ddot(mu, A_2, &s_1);
+
+	//gsl_blas_ddot(&mu.vector, &a_column_1.vector, &q);
+	//gsl_blas_ddot(&mu.vector, &a_column_2.vector, &s_1);
+	s_1 *= y;
+	gsl_blas_ddot(mu, &b.vector, &s_2);
+	//gsl_blas_ddot(&mu.vector, &b.vector, &s_2);
+	s = s_1 + s_2;
+
+	std::cout << "\nq = \n" << q;
+	std::cout << "\ns = \n" << s;
+
+	//auto s = (mu.vector[0] * A.matrix[0, 1] + mu.vector[1] * A.matrix[1, 1] + mu.vector[2] * A.matrix[2, 1]) * y+ dotgsl
+}
+
 
 int main()
 {
+#ifdef HAVE_INLINE
+	cout << " HAVE INLINE " << endl;
+#endif
 	cout << "Hello CMake." << endl;
 
 	double A_data[] = {
@@ -52,5 +112,10 @@ int main()
 	gsl_vector_free(x);
 	gsl_permutation_free(perm);
 
+
+	setup_1d_test();
+
+
 	return 0;
 }
+
