8c53eee3e25639cc8d559de275d172dacc7191b9
Tobias Arens <arenst@hochschule-trier.de>
2022-04-12 13:20:41 +0200

    branch for armadillo tests

---
+++ SteepestDescent/math_utils.h
@@ -0,0 +1,43 @@
+#pragma once
+#include <vector>
+#include <algorithm>
+#include <numeric>
+
+#include <complex>
+#include <armadillo>
+
+
+
+
+namespace math_utils {
+
+	auto get_laguerre_points(const int n) {
+
+	}
+
+	auto lagpts(int n) {
+		/*alpha = 2 * (1:n) - 1;  beta = 1:(n - 1); % 3 - term recurrence coeffs
+			T = diag(beta, 1) + diag(alpha) + diag(beta, -1);% Jacobi matrix
+			[V, D] = eig(T);% eigenvalue decomposition
+			[x, indx] = sort(diag(D));% Laguerre points
+			w = V(1, indx). ^ 2;% Quadrature weights
+			v = sqrt(x).*abs(V(1, indx)).';        % Barycentric weights
+			v = v. / max(v); v(2:2 : n) = -v(2:2 : n);*/
+		std::vector<double> alpha(n);
+		std::iota(std::begin(alpha), std::end(alpha), 1);
+		std::for_each(std::begin(alpha), std::end(alpha), [](auto& x) { x = 2. * x - 1.; });
+		std::vector<double> beta(n-1);
+		
+		std::iota(std::begin(beta), std::end(beta), 0);
+		arma::mat T = arma::diagmat(arma::vec(beta), 1) + arma::diagmat(arma::vec(alpha)) + arma::diagmat(arma::vec(beta), -1);
+		arma::cx_vec eigval;
+		arma::cx_mat eigvec;
+		auto eigen = arma::eig_gen(eigval, eigvec, T);
+		//auto diag = arma::diagvec(eigvec);
+
+
+	}
+
+
+
+}
