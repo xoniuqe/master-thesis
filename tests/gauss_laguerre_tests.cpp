0842fa514731581f79d830b3a6f129bf8b1788bb
Tobias Arens <arenst@hochschule-trier.de>
2022-05-13 14:55:48 +0200

    changed gausse_laguerre impl to armadillo and added a test. Removed unused functions from math_utils

---
+++ tests/gauss_laguerre_tests.cpp
@@ -0,0 +1,13 @@
+#define CATCH_CONFIG_MAIN
+
+#include <catch2/catch.hpp>
+#include <complex>
+#include <armadillo>
+
+#include "../SteepestDescent/gauss_laguerre.h"
+
+
+TEST_CASE("laguerre points", "[gauss_laguerre]") {
+	auto [laguerre_points, x, y] = gauss_laguerre::calculate_laguerre_points_and_weights(10);
+
+}
