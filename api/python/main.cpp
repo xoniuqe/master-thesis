2ed05e8eb823dee8d13a6256c1516c66d6b0bf05
Tobias Arens <arenst@hochschule-trier.de>
2022-06-10 10:11:05 +0200

    fixed includes and added return value

--- api/python/main.cpp
+++ api/python/main.cpp
@@ -1,17 +1,19 @@
 #include <pybind11/pybind11.h>
 #include <pybind11/eigen.h>
 #include <steepest_descent/integrator.h>
+#include <steepest_descent/integral.h>
 
+#include <complex>
 #include <vector>
 #include <armadillo>
 
 namespace py = pybind11;
-
+using namespace std::complex_literals;
 //https://pybind11.readthedocs.io/en/stable/advanced/cast/eigen.html
 
 //double integrate_1d_bulk(std::vector<Eigen::Ref<MatrixType>>)
 
-double integrate_1d(Eigen::Ref<Eigen::Matrix<double, 3,2>> A1, Eigen::Ref<Eigen::Vector3d> b1, Eigen::Ref<Eigen::Vector3d>, Eigen::Ref<Eigen::Vector3d> mu1, double y, int k, double left_split, double right_split) {
+Eigen::dcomplex integrate_1d(Eigen::Ref<Eigen::Matrix<double, 3,2>> A1, Eigen::Ref<Eigen::Vector3d> b1, Eigen::Ref<Eigen::Vector3d>, Eigen::Ref<Eigen::Vector3d> mu1, double y, int k, double left_split, double right_split) {
 	
 	arma::mat  A{ {0, 0}, {1, 0}, {1, 1 } };
 
@@ -29,7 +31,7 @@ double integrate_1d(Eigen::Ref<Eigen::Matrix<double, 3,2>> A1, Eigen::Ref<Eigen:
 		return  -0.090716 + 0.259133i;
 	};
 	integral::integral_1d integral1d(k, integrator, 0.1);
-
-	std::cout << "result2: " << integral1d(A, b, r, mu, y, left_split, right_split) << std::endl;
-	return;
+	auto result = integral1d(A, b, r, mu, y, left_split, right_split);
+	std::cout << "result: " << result << std::endl;
+	return result;
 }
