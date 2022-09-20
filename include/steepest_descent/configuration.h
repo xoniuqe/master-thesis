e84be2f6cc3765196e28c1d96f54ba9167c79843
Tobias Arens <arenst@hochschule-trier.de>
2022-09-20 14:22:42 +0200

    added default values

--- include/steepest_descent/configuration.h
+++ include/steepest_descent/configuration.h
@@ -5,11 +5,11 @@ namespace config {
 
 	struct configuration {
 		double wavenumber_k;
-		double tolerance;
+		double tolerance = 0.1;
 		size_t gauss_laguerre_nodes;
 	};
 
 	struct configuration_2d : public configuration{
-		double y_resolution;
+		double y_resolution = 0.1;
 	};
 }
