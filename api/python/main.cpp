fb41587c49a6b553a0ba5c4c4c0104e4dd9889ff
Tobias Arens <arenst@hochschule-trier.de>
2022-09-20 09:17:45 +0200

    moved pybind to subfolder of python api

--- api/python/main.cpp
+++ api/python/main.cpp
@@ -1,7 +1,10 @@
 #include <pybind11/pybind11.h>
 #include <pybind11/eigen.h>
+#include <pybind11/stl.h>
+
 #include <steepest_descent/integrator.h>
 #include <steepest_descent/integral_1d.h>
+#include <steepest_descent/gauss_laguerre.h>
 
 
 //#include <complex>
@@ -10,13 +13,18 @@
 
 namespace py = pybind11;
 using namespace std::complex_literals;
+
 //https://pybind11.readthedocs.io/en/stable/advanced/cast/eigen.html
 
 // do not forget to install numpy!
 
 //double integrate_1d_bulk(std::vector<Eigen::Ref<MatrixType>>)
 
-Eigen::dcomplex integrate_1d(Eigen::Matrix<double, 3,2> A1, Eigen::Vector3d b1,Eigen::Vector3d r1, Eigen::Vector3d mu1, double y, int k, double left_split, double right_split) {
+
+typedef Eigen::Matrix<double, 3, 2> MatrixD;
+typedef Eigen::Matrix<int, 3, 2> MatrixI;
+
+Eigen::dcomplex integrate_1d(MatrixD A1, Eigen::Vector3d b1, Eigen::Vector3d r1, Eigen::Vector3d mu1, double y, int k, double left_split, double right_split) {
 	
 	arma::mat A(A1.data(), 3, 2, false, false);
 	//arma::mat A{ }
@@ -44,8 +52,32 @@ Eigen::dcomplex integrate_1d(Eigen::Matrix<double, 3,2> A1, Eigen::Vector3d b1,E
 	//return 1.0 + 3i;
 }
 
+auto integrated2d(const std::tuple<MatrixD, Eigen::Vector3d> triangle, const Eigen::Vector3d observation_point, const Eigen::Vector3d direction, const std::vector<double> nodes, const std::vector<double> weights)
+{
+	
+}	
+
+auto integrated2d(const std::tuple <Eigen::Ref<MatrixD>, Eigen::Ref<Eigen::Vector3d>> triangle)
+{
+
+}
+
+
+auto integrate2d(const MatrixD A) -> auto {
+
+}
+
+
 
 PYBIND11_MODULE(stedepy, m) {
 	m.doc() = "integrator test plugin";
 	m.def("integrate_1d", &integrate_1d, "Calculates the 1D complex integral");   
+	m.def("calcualte_laguerre_points", [](const int n) -> auto {
+		return gauss_laguerre::calculate_laguerre_points_and_weights(n);
+	}, "Returns 'n' laguerre nodes and weights");
+	m.def("test", [](const std::vector<double> nodes) {
+		for (auto& node : nodes) {
+			std::cout << node << "\n";
+		}
+		});
 }
