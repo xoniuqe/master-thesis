f7559b4904907379627fd8d899a95c72c2dac079
Tobias Arens <arenst@hochschule-trier.de>
2022-10-09 09:39:27 +0200

    refactored weighted path functions

--- src/path_utils.cpp
+++ src/path_utils.cpp
@@ -26,14 +26,18 @@ namespace path_utils {
 
 
     auto get_weighted_path_1d(const std::complex<double> split_point, const double y, const datatypes::matrix& A, const  arma::vec3& b, const  arma::vec3& r, const double q, const double k, const double s, const datatypes::complex_root complex_root, const std::complex<double> sing_point)->path_function {
-        auto [path, derivative] = get_complex_path(split_point, y, A, b, r, q, complex_root, sing_point);
+        auto Px = math_utils::calculate_P_x(split_point, y, A, b, r);
+        auto path = get_complex_path(split_point, Px, q, complex_root, sing_point);
+        auto derivative = get_path_derivative(path, y, A, b, r, q, complex_root);
         return [=](const double t) -> auto {
             return derivative(t / k) * std::exp(1i * k * (std::sqrt(math_utils::calculate_P_x(split_point, y, A, b, r)) + q * split_point + s)) * (1. / k) * (1. /std::sqrt(math_utils::calculate_P_x(path(t / k), y, A, b, r)));
         };
     }
 
     auto get_weighted_path(const std::complex<double> split_point, const std::complex<double> y, const datatypes::matrix& A, const  arma::vec3& b, const  arma::vec3& r, const double q, const double k, const std::complex<double> s, const datatypes::complex_root complex_root, const std::complex<double> sing_point)->path_function {
-        auto [path, derivative] = get_complex_path(split_point, y, A, b, r, q, complex_root, sing_point);
+        auto Px = math_utils::calculate_P_x(split_point, y, A, b, r);
+        auto path = get_complex_path(split_point, Px, q, complex_root, sing_point);
+        auto derivative = get_path_derivative(path, y, A, b, r, q, complex_root);
         return [=](const double t) -> auto {
             return derivative(t / k) * std::exp(1i * k * (std::sqrt(math_utils::calculate_P_x(split_point, y, A, b, r)) + q * split_point + s)) * (1. / k) * (1. / std::sqrt(math_utils::calculate_P_x(path(t / k), y, A, b, r)));
         };
