5887b591be494344bbee21547eca8e2529b13225
Tobias Arens <arenst@hochschule-trier.de>
2022-10-15 10:54:02 +0200

    help files for matlab (unused)

---
+++ api/matlab/stedepyMatlab_2d.m
@@ -0,0 +1,6 @@
+ stedepyMatlab_2d Running the steepest-descent method for the integral.
+    I = stedepyMatlab_2d(A, b, r, theta, k, nodes, weights, resolution) approximates the 
+    integral other the triangle defined by A and b, for wavelength k, observation point r and direction theta.
+    The parameters nodes and weights are the gauss-laguerre nodes and weights to use.
+    With the 0 > resolution < 1 the number of layers is controlled.
+
