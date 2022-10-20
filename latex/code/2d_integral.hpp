869c344f835cdf55aebcf915cc2d6f8dca59c821
Tobias Arens <arenst@hochschule-trier.de>
2022-10-20 13:49:16 +0200

    fixed header file of latex

--- latex/code/2d_integral.hpp
+++ latex/code/2d_integral.hpp
@@ -20,7 +20,5 @@ namespace integral {
 		integrator::gsl_integrator* integrator;
 		integrator::gsl_integrator_2d* integrator_2d;
 		std::vector<double> nodes, weights;
-
-		auto get_partial_integral(const arma::mat& A, const arma::vec& b, const arma::vec& r, const std::complex<double> sPx, const double q,  const std::complex<double> s, const std::complex<double> c, const double c_0, const double left_split, const double right_split) const ->std::complex<double>;
 	};
 }
