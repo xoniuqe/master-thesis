ec190c582d7e1b2c3c082f8aa3d0f7b6311ad7f4
xoniuqe <arenst@hochschule-trier.de>
2023-02-02 14:25:06 +0100

    Delete latex directory

--- latex/code/configuration2d.cpp
+++
@@ -1,9 +0,0 @@
-struct configuration {
-    double wavenumber_k;
-    double tolerance = 0.1;
-    size_t gauss_laguerre_nodes;
-};
-
-struct configuration_2d : public configuration{
-    double y_resolution = 0.1;
-};
